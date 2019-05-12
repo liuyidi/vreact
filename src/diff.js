bce4a3b335b7249024a0890f8de02a938bfe8d21
liuyidi <liuyidi1993@gmail.com>
2019-05-12 23:46:57 +0800

    Update diff.js

--- src/diff.js
+++ src/diff.js
@@ -2,3 +2,12 @@
 export function diff (dom, vnode, container) {
   return dom
 }
+
+/**
+ * 生命周期
+ */
+function renderComponent (component) {
+  if (!component.base && component.componentWillMount) {
+    component.componentWillMount()
+  }
+}

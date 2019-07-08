69b31bd354eef61e024875ffdeafac1326219572
liuyidi <liuyidi1993@gmail.com>
2019-07-08 23:57:01 +0800

    Update component.js

--- src/component.js
+++ src/component.js
@@ -3,7 +3,18 @@ export function Component (props, context) {
   this.context = context
 }
 
-Component.prototype.setState = function (update, callback) {}
+Component.prototype.setState = function (update, callback) {
+  let s = (this._nextState !== this.state && this._nextState) || (this._nextState = Object.assign({}, this.state));
+  
+  // @TODO
+  
+  if (update === null) return false
+  
+  if (this._vnode) {
+    if (callback) this._renderCallbacks.push(callback)
+    enqueueRender(this)
+  }
+}
 
 Component.prototype.forceUpdate = function (callback) {}
 
